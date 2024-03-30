72f4053a50be58780ffcea843f22ef5e9a42af39
raychuranirav <123540547+raychuranirav@users.noreply.github.com>
2024-03-30 20:13:14 +0530

    Buggy Code

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end("WELCOME TO THE WORLD OF CLOUD !!!\n'");
+    // Intentional bug: Trying to access an undefined variable
+    res.end("WELCOME TO THE WORLD OF CLOUD !!!\n" + someUndefinedVariable);
 });
 server.listen(port, () => {
     console.log(`Server running on port ${port}/`);
 });
-
-// Intentional bug: Missing closing quote in the response string
 module.exports = server;
