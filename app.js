685ca61bb8d0c75da65078f4aae93be72b92adff
raychuranirav <123540547+raychuranirav@users.noreply.github.com>
2024-03-30 20:44:17 +0530

    Buggy code

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    // Intentional bug: Trying to access an undefined variable
-    res.end("WELCOME TO THE WORLD OF CLOUD !!!\n" + someUndefinedVariable);
+    // Intentional bug: Hardcoded response message
+    res.end("WELCOME TO THE WORLD OF CLOUD !!!\n");
 });
 server.listen(port, () => {
     console.log(`Server running on port ${port}/`);
